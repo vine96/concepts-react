513f5250100226c37f9ab528e559b2926c1c8b3f
vine96 <viniciuspecci@hotmail.com>
2024-08-10 22:59:21 -0300

    React Icons

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -1,5 +1,17 @@
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa"
+import styles from "./Footer.module.css"
+
 function Footer(){
-    return <footer>Rodapé</footer>
+    return(
+        <footer>
+            <ul className={styles.social_list}>
+                <li><FaFacebook/></li>
+                <li><FaInstagram/></li>
+                <li><FaYoutube/></li>
+            </ul>
+            <p>Nosso rodapé</p>
+        </footer>
+    )
 }
 
 export default Footer
